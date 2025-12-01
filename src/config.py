e5eb7c3da9d12d4bf785fbcb0e038ac8522922e7
SamuelReinaldoRamirez <samuel.reinaldo.ramirez@gmail.com>
2025-12-01 15:23:14 +0100

    $ git status On branch aissam-sam-collect-data Changes not staged for commit:   (use git add <file>... to update what will be committed)   (use git restore <file>... to discard changes in working directory)         modified:   data_collection.log         modified:   src/config.py
    Untracked files:
      (use git add <file>... to include in what will be committed)
            src/11_25_csv_to_df_and_describe.ipynb
            src/describe_complet.csv
            src/describe_complet.xlsx

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
-API_KEY = "RGAPI-b7596442-3c60-47eb-a545-0aa6199e3f0a"  # Remplacez par votre clé Riot
+API_KEY = "RGAPI-faf59688-420c-4d6d-b956-1d20b35bcc53"  # Remplacez par votre clé Riot
 REGION = "euw1"            # Région du shard LoL (euw1, na1, kr, etc.)
 QUEUE = "RANKED_SOLO_5x5"
-TIER = "DIAMOND"
+TIER = "MASTER"
 DIVISION = "I"
